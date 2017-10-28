1063b80622a3182cd0c15b777df6d55a57304a89
shree <shreedhar16197@iiitd.ac.in>
2017-10-28 17:38:34 +0530

    Added Menu Items in Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Menu;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -34,11 +37,17 @@ public class Game extends Application {
 		players[1]=new Player(Color.GREEN);
 		players[2]=new Player(Color.BLUE);
 		grid=new Grid(sceneWidth,sceneHeight,border,n,m,number_players);
+
+		Menu options = new Menu("Options");
+		Menu Undo = new Menu("Undo");
+		options.getItems().addAll(new MenuItem("Restart"),new MenuItem("Menu"));
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(options,Undo);
 		
 		
-		vBox.getChildren().add(grid);
+		vBox.getChildren().addAll(menuBar,grid);
 		vBox.setStyle("-fx-background-color: #1e252a");
-		Scene scene = new Scene(vBox,sceneWidth+border,sceneHeight+border,Color.BLACK);
+		Scene scene = new Scene(vBox,sceneWidth+border,sceneHeight+border+25,Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
